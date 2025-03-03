c66b9f0ad5aa92fac5bb09df7d2a2f4693926d8a
Amit <amittiwari79099@gmail.com>
2025-03-03 12:27:21 +0530

    Switch & Lops

--- Intro_JS/intro.js
+++ Intro_JS/intro.js
@@ -32,9 +32,93 @@ if (isUsserLoggedIn) {
 // syntax :variable = condition ? true :  false
 const userStatus = true ? console.log("send user to home page") : console.log("send user to login page");
 
-// Loops
+/* Loops
+1.) For 
+2.) while
+3.) do-while */
+
 // for loop 
 for (let i=1;i<=20;i++)
 {
     console.log(i);
+}
+
+//while loop
+
+let j = 5;
+while (j <=20) {
+    console.log(j);
+    j++  
+}
+// do-while loop
+//first do work then check the condition 
+do{
+    console.log(j);
+    j++
+}
+while(j <=20)
+
+// switch statement
+const day = 12;
+switch (day) {
+    case 1:
+        console.log("Monday");
+        break;
+    case 2:
+        console.log("Tuesday");
+        break;
+    case 3:
+        console.log("Wednesday");
+        break;
+    case 4:
+        console.log("Thursday");
+        break;
+    case 5:
+        console.log("Friday");
+        break;
+    case 6:
+        console.log("Saturday");
+        break;
+    case 7:
+        console.log("Sunday");
+        break;
+    default:
+        console.log("Invalid day");
+}
+
+//write a program to check day is woking or not 
+//using if else it becoe hactic and dificult so we can use here Switch statement
+
+const date = "Monday"
+switch (date) {
+    case "Monday":
+        console.log("Wow! it's a working Day");
+        break;
+
+    case "Tuesday":
+        console.log("Wow! it's a working Day");
+        break;
+        
+    case "Wednesday":
+        console.log("Wow! it's a working Day");
+        break;
+    
+    case "Thursday":
+        console.log("Wow! it's a working Day");
+        break;
+
+    case "Friday":
+        console.log("Wow! it's a working Day");
+        break;
+    
+    case "Saturday":
+        console.log("Wow! it's a working Day");
+        break;
+    
+    case "Sunday":
+        console.log("Today is Holyday");
+
+    default:
+        console.log("invalid day");
+        break;
 }
